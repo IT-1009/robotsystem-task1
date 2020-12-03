a2d015e3d0d0c79af4e9ccc71c4fcb2aec16589b
IT-1009 <72001805+IT-1009@users.noreply.github.com>
2020-12-03 13:30:45 +0900

    Update myled.c

--- program/myled.c
+++ program/myled.c
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0
+// /*
+//  * Copyright (C) 2020 Ryuichi Ueda and Tsubasa Ito.  All rights reserved.
+//
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
